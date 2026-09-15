09d88585c4e125f0c7660a803e46d21f24a24efd
zeynep bas <101248567+zeynepbass@users.noreply.github.com>
2026-09-15 22:17:07 +0300

    summary

--- client/src/features/messages/repositories/message.repository.js
+++ client/src/features/messages/repositories/message.repository.js
@@ -1,53 +1,31 @@
+
 import messageProvider from "@/providers/message.provider";
 import messageAdapter from "../adapters/message.adapter";
 
-export function getMessages() {
-    return {
-        async getMessages(gonderenId, aliciId) {
-            const response =
-                await messageProvider.getMessages(
-                    gonderenId,
-                    aliciId
-                );
+export async function getMessages(gonderenId, aliciId) {
+    const response = await messageProvider.getMessages(
+        gonderenId,
+        aliciId
+    );
 
-            return response.map(messageAdapter);
-        },
-    };
+    return response.map(messageAdapter);
 }
 
-export function getUser() {
-    return {
-        async getUser(userId) {
-            return await messageProvider.getUser(userId);
-        },
-    };
+export async function getUser(userId) {
+    return await messageProvider.getUser(userId);
 }
 
-export function getUsers() {
-    return {
-        async getUsers() {
-            return await messageProvider.getUsers();
-        },
-    };
+export async function getUsers() {
+    return await messageProvider.getUsers();
 }
 
-export function getConversations() {
-    return {
-        async getConversations(userId) {
-            return await messageProvider.getConversations(
-                userId
-            );
-        },
-    };
+export async function getConversations(userId) {
+    return await messageProvider.getConversations(userId);
 }
 
-export function getMessageData() {
-    return {
-        async getMessageData(currentId, targetId) {
-            return await messageProvider.getMessageData(
-                currentId,
-                targetId
-            );
-        },
-    };
+export async function getMessageData(currentId, targetId) {
+    return await messageProvider.getMessageData(
+        currentId,
+        targetId
+    );
 }
