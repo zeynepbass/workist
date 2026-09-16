ccfc33486e06d8e5917444b731629d80f19f64c6
zeynep bas <101248567+zeynepbass@users.noreply.github.com>
2026-09-16 18:26:53 +0300

    ***

--- server/controllers/ilanlarim.js
+++ server/controllers/ilanlarim.js
@@ -48,8 +48,7 @@ const getPost = async (req, res) => {
 
 const getPosts = async (req, res) => {
   try {
-      const userId = req.user.id;
-
+    const userId = req.user.id;
       const posts = await Ilanlarim.find({ userId });
 
       return res.status(200).json(posts);
